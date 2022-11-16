083568a0db17267294fed6d4502e04dbafb28c94
ibizugbe <abrahamibizugbe@gmail.com>
2022-11-16 06:07:52 +0100

    update route path

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
+  resources :groups do
+    resources :transactions
+  end
+
+  resources :users
   root "user#index"
 end
