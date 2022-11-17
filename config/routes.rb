ec45db665aa88e07e3ab9a21c4c92036365decc3
ibizugbe <abrahamibizugbe@gmail.com>
2022-11-17 09:41:29 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
+
   resources :groups do
-    resources :transactions
+    resources :entities
   end
 
   resources :users
