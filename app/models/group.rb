78df001d393922075649ccddcb745ac407de2822
ibizugbe <abrahamibizugbe@gmail.com>
2022-11-18 15:43:01 +0100

    update group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :transactions, dependent: :destroy
+  has_many :entities, dependent: :destroy
 
   validates :name, presence: true
   validates :icon, presence: true
+
+  def total_amount
+    entities.sum(:amount)
+  end
 end
